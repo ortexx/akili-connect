db8d36060b939a48607245e7af1df0f192783931
Alxandr Balasyan <mywebstreet@gmail.com>
2017-06-25 21:22:55 +0300

    jsdom memort leak v1

--- render.js
+++ render.js
@@ -9,28 +9,40 @@ module.exports = function(dom, url, indexUrl,  _options) {
   options = Object.assign({}, options, _options);
 
   let window = dom.window;
-  let timeout;
 
   for(let key in polyfill) {
     polyfill[key](window);
+  }
+
+  function close() {
+    let html = dom.serialize();
+    let timeout = setTimeout(() => {
+      window.close();
+      global.gc && global.gc();
+      clearTimeout(timeout);
+    }, 1000);    
+
+    return html;
   }
   
   return Promise.resolve(options.onDomInit(dom)).then(() => {
     url && window.history.pushState(null, '', url);
 
     return new Promise((resolve) => {
+      let timeout;
+
       if(options.timeout) {
         timeout = setTimeout(() => {
           clearTimeout(timeout);
           console.warn('Server rendering has been stopped by timeout');
-          resolve(dom.serialize());
+          resolve(close());
         }, options.timeout);
       }
 
       window.addEventListener('akili-init', () => {
         timeout && clearTimeout(timeout);
-        window.document.documentElement.setAttribute('akili-server', indexUrl);
-        resolve(dom.serialize());
+        window.document.documentElement.setAttribute('akili-server', indexUrl);          
+        resolve(close());
       });
     });
   });
