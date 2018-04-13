99cfd53e2dba252e9489e0136fcf84e010f5fdbc
Alexandr Balasyan <mywebstreet@gmail.com>
2018-04-13 19:40:09 +0300

    better random for index url

--- middleware.js
+++ middleware.js
@@ -14,7 +14,8 @@ exports.route = function (req, res, next) {
   let host = this.options.host || req.connection.localAddress.replace(/^::1/, '127.0.0.1').replace(/^::ffff:/, '');
   let protocol = this.options.protocol || req.connection.encrypted? 'https': 'http';
   let port = this.options.port || req.connection.localPort;  
-  let url = this.options.indexUrl || urlLib.format({ hostname: host, port: port, protocol: protocol }) + '/' + crypto.randomBytes(24).toString('hex');  
+  let serverUrl = urlLib.format({ hostname: host, port: port, protocol: protocol });
+  let url = this.options.indexUrl || serverUrl + '/' + crypto.randomBytes(24).toString('hex').split('').join('_-/');
   
   jsdom.JSDOM.fromURL(url, options).then(dom => {
     return render(dom, req.originalUrl, this.options).then(html => res.send(html));
